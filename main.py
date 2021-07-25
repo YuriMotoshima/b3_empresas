2f4199a48f92b8f9ab833130d47127186ca50ba4
YuriMotoshima <57924764+YuriMotoshima@users.noreply.github.com>
2021-07-25 17:13:27 -0300

    Update

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import pandas as pd
 wb = configChromeDriver(webVisible=False)
 wb.get(url="http://www.b3.com.br/pt_br/produtos-e-servicos/negociacao/renda-variavel/empresas-listadas.htm")
 
-wb.switch_to.frame("bvmf_iframe")
-# wb.switch_to.frame("_hjRemoteVarsFrame")
-# wb.find_element_by_id("onetrust-accept-btn-handler").click()
+wb.find_element_by_id("onetrust-accept-btn-handler").click()
 
-select_emp = wb.find_element_by_class_name("inline-list-letra").find_elements_by_tag_name("a")
+wb.switch_to.frame("bvmf_iframe")
+
+select_emp = [n.text for n in wb.find_element_by_class_name("inline-list-letra").find_elements_by_tag_name("a")]
 
 for n in select_emp:
-    n.click()
+    wb.find_element_by_link_text(n).click()
     check_exists_elements(wb=wb, method="css_selector", element="table[id='ctl00_contentPlaceHolderConteudo_BuscaNomeEmpresa1_grdEmpresa_ctl01']")
 
     table = wb.find_element_by_css_selector("table[id='ctl00_contentPlaceHolderConteudo_BuscaNomeEmpresa1_grdEmpresa_ctl01']").get_attribute("outerHTML")
     df = pd.read_html(table, header=0, index_col=False)[0]
-    wb.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-    print()
+    print(df.shape)
     wb.find_element_by_id("ctl00_botaoNavegacaoVoltar").click()
 
 wb.quit()
